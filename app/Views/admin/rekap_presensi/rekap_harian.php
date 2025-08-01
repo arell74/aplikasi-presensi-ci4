b926a2b4bd13308f2b2e8cbf777b5850bd745872
arell74 <farelfauzan36@gmail.com>
2025-08-01 22:51:22 +0700

    feat: Add Date Filtering to rekap_harian
    - Enhance rekap_harian view for improved date selection and data display.
    
    - Implement date filtering logic in RekapPresensi Controller.

--- app/Views/admin/rekap_presensi/rekap_harian.php
+++ app/Views/admin/rekap_presensi/rekap_harian.php
@@ -3,13 +3,23 @@
 <?= $this->section('content') ?>
 
 <form class="row g-3">
-  <div class="col-auto">
-    <input type="date" class="form-control" name="filter_tanggal">
-  </div>
-  <div class="col-auto">
-    <button type="submit" class="btn btn-primary mb-3">Tampilkan</button>
-  </div>
+    <div class="col-auto">
+        <input type="date" class="form-control" name="filter_tanggal">
+    </div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary">Tampilkan</button>
+    </div>
 </form>
+
+<div class="my-2">
+    <span>Menampilkan Data
+        <?php if ($tanggal) : ?>
+            <?= date('d F Y', strtotime($tanggal)) ?>
+        <?php else : ?>
+            <?= date('d F Y'); ?>
+        <?php endif; ?>
+    </span>
+</div>
 
 <table class="table table-striped table-bordered table-hover">
     <tr>
@@ -22,51 +32,57 @@
         <th>Total Keterlambatan</th>
     </tr>
 
-    <?php
-    $no = 1;
-    foreach ($rekap_harian as $rekap) :  ?>
+    <?php if ($rekap_harian) : ?>
         <?php
-        // menghitung jumlah jam kerja
-        $timestamp_jam_masuk = strtotime($rekap['jam_masuk']);
-        $timestamp_jam_keluar = strtotime($rekap['jam_keluar']);
+        $no = 1;
+        foreach ($rekap_harian as $rekap) :  ?>
+            <?php
+            // menghitung jumlah jam kerja
+            $timestamp_jam_masuk = strtotime($rekap['jam_masuk']);
+            $timestamp_jam_keluar = strtotime($rekap['jam_keluar']);
 
-        $selisih = $timestamp_jam_keluar - $timestamp_jam_masuk;
-        $jam = floor($selisih / 3600);
-        $selisih -= $jam * 3600;
-        $menit = floor($selisih / 60);
+            $selisih = $timestamp_jam_keluar - $timestamp_jam_masuk;
+            $jam = floor($selisih / 3600);
+            $selisih -= $jam * 3600;
+            $menit = floor($selisih / 60);
 
-        // menghitung total jam keterlambatan
-        $jam_masuk_pegawai = strtotime($rekap['jam_masuk']);
-        $jam_masuk_kantor = strtotime($rekap['jam_masuk_kantor']);
+            // menghitung total jam keterlambatan
+            $jam_masuk_pegawai = strtotime($rekap['jam_masuk']);
+            $jam_masuk_kantor = strtotime($rekap['jam_masuk_kantor']);
 
-        $selisih_terlambat = $jam_masuk_kantor - $jam_masuk_pegawai;
-        $jam_terlambat = floor($selisih_terlambat / 3600);
-        $selisih_terlambat -= $jam_terlambat * 3600;
-        $menit_terlambat = floor($selisih_terlambat / 60);
+            $selisih_terlambat = $jam_masuk_kantor - $jam_masuk_pegawai;
+            $jam_terlambat = floor($selisih_terlambat / 3600);
+            $selisih_terlambat -= $jam_terlambat * 3600;
+            $menit_terlambat = floor($selisih_terlambat / 60);
 
-        ?>
+            ?>
+            <tr>
+                <td><?= $no++; ?></td>
+                <td><?= $rekap['nama']; ?></td>
+                <td><?= date('d F Y', strtotime($rekap['tanggal_masuk'])); ?></td>
+                <td><?= $rekap['jam_masuk']; ?></td>
+                <td><?= $rekap['jam_keluar']; ?></td>
+                <td>
+                    <?php if ($rekap['jam_keluar'] == '00:00:00') : ?>
+                        Proses Kerja
+                    <?php else : ?>
+                        <?= $jam . ' Jam ' . $menit . ' Menit '; ?>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <?php if ($jam_terlambat < 0 || $menit_terlambat < 0) : ?>
+                        <a href="#" class="badge bg-success">On Time</a>
+                    <?php else : ?>
+                        <?= $jam_terlambat . ' Jam ' . $menit_terlambat . ' Menit '; ?>
+                    <?php endif; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php else : ?>
         <tr>
-            <td><?= $no++; ?></td>
-            <td><?= $rekap['nama']; ?></td>
-            <td><?= date('d F Y', strtotime($rekap['tanggal_masuk'])); ?></td>
-            <td><?= $rekap['jam_masuk']; ?></td>
-            <td><?= $rekap['jam_keluar']; ?></td>
-            <td>
-                <?php if ($rekap['jam_keluar'] == '00:00:00') : ?>
-                    Proses Kerja
-                <?php else : ?>
-                    <?= $jam . ' Jam ' . $menit . ' Menit '; ?>
-                <?php endif; ?>
-            </td>
-            <td>
-                <?php if ($jam_terlambat < 0 || $menit_terlambat < 0) : ?>
-                    <a href="#" class="badge bg-success">On Time</a>
-                <?php else : ?>
-                    <?= $jam_terlambat . ' Jam ' . $menit_terlambat . ' Menit '; ?>
-                <?php endif; ?>
-            </td>
+            <td colspan="7">Data Tidak Tersedia...</td>
         </tr>
-    <?php endforeach; ?>
+    <?php endif; ?>
 </table>
 
 <?= $this->endSection() ?>
